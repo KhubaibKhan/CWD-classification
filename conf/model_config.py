28944fdb42311d282ced87ac9655d080ccda51f2
Khubaib <khubaibtakkar@gmail.com>
2023-11-12 16:10:16 +0900

    Generalized model pipeline

--- conf/model_config.py
+++ conf/model_config.py
@@ -3,7 +3,7 @@ import yaml
 
 
 # yaml file path
-yaml_path = "../models/models.yaml"
+yaml_path = "models/models.yaml"
 
 model_dict = {}
 
@@ -11,4 +11,6 @@ with open(yaml_path, 'r') as f:
     temp_dict = yaml.load(f.read(), Loader=yaml.FullLoader)
     model_dict.update(temp_dict)
 
+# convert keys to values and vice versa
+model_dict = {v: k for k, v in model_dict.items()}
 # %%
